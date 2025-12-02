5930398946ff1f31faccf4dfcc92f6eb466447f7
Kauan Iasin <134458062+SrKain@users.noreply.github.com>
2025-12-02 12:08:52 +0000

    ok

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.d842d11c388d4c88b347fe90382d060b',
+  appName: 'OceanPomodoro',
+  webDir: 'dist'
+};
+
+export default config;
